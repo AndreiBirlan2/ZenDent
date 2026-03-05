4276fbe47970f52a8cec49f70c578cab86981294
AndreiBirlan2 <birlan.andrei93@gmail.com>
2026-03-05 09:22:05 +0200

    price & people changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,14 +12,19 @@ export class AboutComponent {
 
   employees = [
     {
-      name: 'Dr. Rizan Andra Corina',
+      name: 'Dr. Rizan Andra Corina - medic specialist stomatologie generală',
       photo: 'assets/Corina2.PNG',
       description: 'Medic specialist în stomatologie generală, dedicat, pasionat de sănătatea orală și de starea de bine a pacienților mei. Mă preocup nu doar de dantura acestora, ci și de confortul și echilibrul lor psihic, creând un mediu plăcut și relaxant. Îmi place să salvez și să restaurez dinții, asigurându-le atât funcționalitate, cât și estetică. Investesc constant în perfecționarea mea profesională pentru a oferi cele mai bune soluții personalizate și cele mai moderne tratamente.'
     },
     {
-      name: 'Dr. Roxana Lăcrămioara Groza',
-      photo: 'assets/Roxana1.PNG',
-      description: 'Medic stomatolog generalist cu pasiune pentru sănătatea dentară și o abordare empatică. Mă dedic să îți rezolv orice problemă dentară, oferindu-ți tratamente personalizate și o experiență plăcută, fără stres. Cred că fiecare zâmbet merită să fie sănătos și încrezător, iar scopul meu este să îți aduc înapoi zâmbetul cu profesionalism și grijă. Cu răbdare și atenție, îți voi oferi cele mai bune soluții pentru un confort pe termen lung.'
+      name: 'Dr. Alex Ciupitu - medic specialist stomatologie generală',
+      photo: 'assets/Alex.jpeg',
+      description: 'Pentru mine, stomatologia este mai mult decât o profesie — este un dialog subtil între știință și grijă, între estetică și echilibru interior. Privesc sănătatea orală ca parte din armonia întregului trup, o expresie a stării noastre de bine și a încrederii în sine. Fiecare pacient este unic, iar fiecare zâmbet are propria poveste. Mă ghidez după empatie, răbdare și dorința sinceră de a reda nu doar funcția, ci și frumusețea naturală a fiecărui zâmbet. Cred cu tărie că adevăratul profesionalism înseamnă nu doar precizie și competență, ci și capacitatea de a vedea omul dincolo de tratament — de a trata cu inimă, nu doar cu mâini.'
+    },
+    {
+      name: 'Dr. Denisa Elena Baltaretu medic stomatolog/implantolog',
+      photo: 'assets/image.png',
+      description: 'Specializată în fațetele dentare, lucrări complexe de zirconiu, implanturi dentare și reabilitări complete de arcade maxilare. Inovația și perfecțiunea definesc munca sa.'
     }
   ];
 
